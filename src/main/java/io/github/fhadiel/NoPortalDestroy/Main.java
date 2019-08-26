c77bb3af9ead3fba77ec06be3c577073dee9bdf6
GrimReaperFloof <GrimReaperFloof@users.noreply.github.com>
2019-08-26 19:42:58 +0200

    Disallow certain items from dispensing, 1.13 compatibility, fixed grammar and spelling mistakes
    - Added 1.13 support
    - Fixed the grammar and spelling mistakes
    - Better variable names, hopefully
    - Added ability to choose what items are not allowed to be dispensed into protected blocks (see config)
    - Fixed NullPointerException that can occur when having invalid Materials in the config

--- src/main/java/io/github/fhadiel/NoPortalDestroy/Main.java
+++ src/main/java/io/github/fhadiel/NoPortalDestroy/Main.java
@@ -11,25 +11,32 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 	ArrayList<Material> preventBlockBreak = new ArrayList<>();
+	ArrayList<Material> preventItemDispense = new ArrayList<>();
 	FileConfiguration config = getConfig();
+	
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
 		saveDefaultConfig();
-		updateConfig(getConfig().getStringList("preventbreak"));
-		log("Prevent these block from breaking " + preventBlockBreak.toString());
+		preventBlockBreak = updateMaterials(config.getStringList("preventbreak"));
+		preventItemDispense = updateMaterials(config.getStringList("preventdispensing"));
+		
+		log("Preventing these blocks from breaking: " + preventBlockBreak.toString());
+		log("Preventing these items from dispensing: " + preventItemDispense.toString());
 	}
 
 	@EventHandler
 	public void onPlaceWater(PlayerBucketEmptyEvent e) {
 		Block clickedBlock = e.getBlockClicked();
-		Material replacedBlock = clickedBlock.getRelative(e.getBlockFace()).getType();
-		if (isEndPortal(replacedBlock)) {
+		Block replacedBlock = clickedBlock.getRelative(e.getBlockFace());
+		if (isPreventBreakBlock(replacedBlock)) {
 			e.setCancelled(true);
+			if(config.getBoolean("logging")) log("Prevented " + e.getPlayer().getName() + " from breaking " + replacedBlock.getType().name() + " using a bucket at " + replacedBlock.getLocation().getBlockX() + " " + replacedBlock.getLocation().getBlockY() + " " + replacedBlock.getLocation().getBlockZ());
 		}
 	}
 
@@ -38,35 +45,43 @@ public class Main extends JavaPlugin implements Listener {
 		if (e.getBlock().getType().equals(Material.DISPENSER)) {
 			Dispenser dispenser = (Dispenser) e.getBlock().getState().getData();
 			Block inFrontOfDispenser = e.getBlock().getRelative(dispenser.getFacing());
-			if (isEndPortal(inFrontOfDispenser.getType())) {
+			ItemStack itemInDispenser = e.getItem();
+			if (isPreventBreakBlock(inFrontOfDispenser) && isPreventDispenseItem(itemInDispenser)) {
 				e.setCancelled(true);
+				if(config.getBoolean("logging")) log("Prevented dispenser from breaking " + inFrontOfDispenser.getType().name() + "using " + itemInDispenser.getType().name() + " at " + inFrontOfDispenser.getLocation().getBlockX() + " " + inFrontOfDispenser.getLocation().getBlockY() + " " + inFrontOfDispenser.getLocation().getBlockZ());
 			}
 		}
-
 	}
-
-	public void updateConfig(List<String> blocks) {
-		ArrayList<Material> materialBlock = new ArrayList<>();
-		for (String block : blocks) {
-			Material b = Material.getMaterial(block);
-			if (!(b instanceof Material)) {
-				log("There's no block called " + b.name());
+	
+	public ArrayList<Material> updateMaterials(List<String> configList) {
+		ArrayList<Material> materialArray = new ArrayList<>();
+		for (String material : configList) {
+			Material m = Material.getMaterial(material);
+			if (!(m instanceof Material)) {
+				log("There's no material called " + material);
 			} else {
-				materialBlock.add(b);
+				materialArray.add(m);
 			}
 		}
-		preventBlockBreak = materialBlock;
+		
+		return materialArray;
 	}
 
-	public boolean isEndPortal(Material replacedBlock) {
-		if (preventBlockBreak.contains(replacedBlock)) {
-			log("someone try to break " + replacedBlock.name());
+	public boolean isPreventBreakBlock(Block replacedBlock) {
+		if (preventBlockBreak.contains(replacedBlock.getType())) {
 			return true;
 		}
-
 		return false;
 	}
+	
+	public boolean isPreventDispenseItem(ItemStack item) {
+		if (preventItemDispense.contains(item.getType())) {
+			return true;
+		}
+		return false;
+	}
+	
 	public void log(String msg) {
-		if(config.getBoolean("logging")) getLogger().info(msg);
+		getLogger().info(msg);
 	}
 }
